622d3222d65214468a369f27f7849d1a2fde8ae3
Povpanha28 <sengkim28140@gmail.com>
2025-12-31 13:39:06 +0700

    model

---
+++ lib/models/deck.dart
@@ -0,0 +1,22 @@
+import 'package:flashcard_app/models/flashcard.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = Uuid();
+
+class Deck {
+  final String id;
+  final String title;
+  final String description;
+  final List<Flashcard> cards;
+
+  Deck({required this.title, required this.description, required this.cards})
+    : id = const Uuid().v4();
+
+  void addCard(Flashcard newCard) {
+    cards.add(newCard);
+  }
+
+  void removeCard(String cardId) {
+    cards.removeWhere((card) => card.id == cardId);
+  }
+}
---
+++ lib/models/flashcard.dart
@@ -0,0 +1,12 @@
+import 'package:uuid/uuid.dart';
+
+var uuid = Uuid();
+
+class Flashcard {
+  final String id;
+  final String question;
+  final String answer;
+
+  Flashcard({required this.question, required this.answer})
+    : id = const Uuid().v4();
+}
