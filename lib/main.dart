2cbf331cac8686dfb8345bac98a864d87e7b3fd9
Povpanha28 <sengkim28140@gmail.com>
2026-01-02 21:26:23 +0700

    hmm

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
-import 'package:flashcard_app/ui/screens/welcome.dart';
 import 'package:flutter/material.dart';
+import 'ui/theme/app_theme.dart';
+import 'ui/screens/welcome.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const FlashLearnApp());
 }
 
-class MyApp extends StatelessWidget {
+class FlashLearnApp extends StatelessWidget {
+  const FlashLearnApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Welcome()),
+      title: 'FlashLearn',
+      theme: AppTheme.lightTheme,
+      home:  Welcome(),
     );
   }
 }
